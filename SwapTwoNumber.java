423c61b9361d1fbdc479e3a9daf51b6451fe9529
YashSingh699 <yash21int@gmail.com>
2026-03-15 23:09:39 +0530

    Today solved few problem

---
+++ SwapTwoNumber.java
@@ -0,0 +1,13 @@
+public class SwapTwoNumber {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int temp = a;
+
+        a = b;
+        b = temp;
+        
+        System.out.println("swapping: a="+a+ ", b="+b);
+
+    }
+}
